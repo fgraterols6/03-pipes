c90627819f44ce9a30cc4503edab6265d40580d6
“Fabiana <fabiana.graterol@globant.com>
2022-05-24 13:52:20 -0400

    Pipes in Angular 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+    nombre    : string  = 'Fabiana';
+    nombre2    : string  = 'fabIana gRAterol salaZAr';
+    arreglo   : number[] = [1,2,3,4,5,6,7,8,9,10];
+    nombres   : string[] = ['Andrea','Valeria','Fabiana','Maria','Fabiola', 'Paola'];
+    PI        : number = Math.PI;
+    porcentaje: number = 0.24545;
+    salario   : number = 42153.3;
+    fecha     : Date  = new Date();
+    idioma    : string = 'es';
+    activar   : boolean = true;
+
+    videoUrl  : string = 'https://www.youtube.com/embed/e4rNoUQdfQY';
+
+    valorPromesa = new Promise<string> ( (resolve) => {
+      setTimeout(() => {
+        resolve('llego la data')
+      }, 4500)
+    });
+
+    heroe = {
+      nombre: 'Logan',
+      clave: 'Wolverine',
+      edad: 300,
+      direccion: {
+        calle: 'First Avenue',
+        casa: 20
+      }
+    }
+
+    changeLang( lang: string ) {
+      this.idioma = lang;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import fr from '@angular/common/locales/fr';
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+
+
+
+registerLocaleData(es);
+registerLocaleData(fr);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
